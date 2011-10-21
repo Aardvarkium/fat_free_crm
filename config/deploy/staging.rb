5acd8257b763d59bca8d18a311a41856aad86a5a
john <john@john-Pangolin-Performance.(none)>
2011-10-21 16:31:37 +0530

    update cap script

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,5 @@
 set :user, 'deploy'
-server "crm.webdev.thrasys.com", :app, :web, :db, :primary => true
+server "webdev.thrasys.com", :app, :web, :db, :primary => true
 
 set :rails_env, 'development'
 set :passenger_port, 3061
